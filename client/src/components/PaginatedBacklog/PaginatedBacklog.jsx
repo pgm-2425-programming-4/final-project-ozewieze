46498b4d0749f9c9fa54af51fa174c9a90d92b11
ozewieze <stefballyn@hotmail.com>
2025-05-22 18:49:39 +0200

    added queries to the components folder

--- client/src/components/PaginatedBacklog/PaginatedBacklog.jsx
+++ client/src/components/PaginatedBacklog/PaginatedBacklog.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react';
 import { useQuery } from '@tanstack/react-query';
 import { Backlog } from './Backlog/Backlog';
 import { Pagination } from './Pagination/Pagination';
-import { getTasks } from '../../queries/getTasks';
+import { getTasks } from '../queries/getTasks';
 
 function PaginatedBacklog() {
   const [currentPage, setCurrentPage] = useState(1);
@@ -21,23 +21,6 @@ function PaginatedBacklog() {
 
   if (isPending) return 'Loading...';
   if (error) return 'An error has occurred';
-  // useEffect(() => {
-  //   fetch(
-  //     `${API_URL}/tasks?populate=*&filters[statuses][name][$eq]=Backlog&pagination[page]=${currentPage}&pagination[pageSize]=${pageSize}`,
-  //     {
-  //       headers: { Authorization: `Bearer ${API_TOKEN}` },
-  //     }
-  //   )
-  //     .then(data => data.json())
-  //     .then(jsondata => {
-  //       if (currentPage > jsondata.meta.pagination.pageCount) {
-  //         setCurrentPage(jsondata.meta.pagination.pageCount);
-  //       }
-  //       console.log(jsondata);
-  //       setTasks(jsondata.data);
-  //       setPageCount(jsondata.meta.pagination.pageCount);
-  //     });
-  // }, [currentPage, pageSize]);
   const tasks = data?.data || [];
   const pageCount = data?.meta?.pagination?.pageCount || 1;
   return (
