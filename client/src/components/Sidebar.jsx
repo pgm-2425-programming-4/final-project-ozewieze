71fc8bfa71ff66fe2f865b23197166ad4a18924d
ozewieze <stefballyn@hotmail.com>
2025-06-02 21:53:10 +0200

    added a header and a sidebare component

---
+++ client/src/components/Sidebar.jsx
@@ -0,0 +1,15 @@
+export function Sidebar() {
+  return (
+    <aside className="sidebar">
+      <h1>PROJECTS</h1>
+      <ul>
+        <li>
+          <button className="btn sidebar-task">PGM3</button>
+        </li>
+        <li>
+          <button className="btn sidebar-task">PGM4</button>
+        </li>
+      </ul>
+    </aside>
+  );
+}
--- client/src/routes/__root.jsx
+++ client/src/routes/__root.jsx
@@ -2,49 +2,17 @@ import * as React from 'react';
 import { createRootRoute, Link, Outlet } from '@tanstack/react-router';
 import { TanStackRouterDevtools } from '@tanstack/react-router-devtools';
 import '../../main.css';
+import { Sidebar } from '../components/Sidebar';
+import { Header } from '../components/Header';
 
 export const Route = createRootRoute({
   component: () => (
     <React.Fragment>
       <div className="app-container">
-        <aside className="sidebar">
-          <h1>PROJECTS</h1>
-          <ul>
-            <li>
-              <button className="btn sidebar-task">PGM3</button>
-            </li>
-            <li>
-              <button className="btn sidebar-task">PGM4</button>
-            </li>
-          </ul>
-        </aside>
+        <Sidebar />
+
         <div className="content-container">
-          <header className="board-header">
-            <form action="#">
-              <select className="select" name="tags" id="tag-select">
-                <option value="infra">Infra</option>
-                <option value="back-end">Back-end</option>
-                <option value="front-end">Front-end</option>
-                <option value="documentation">Documentation</option>
-              </select>
-              <input
-                className="input"
-                type="text"
-                name="search"
-                placeholder="Search by description"
-                aria-label="Search tasks"
-              />
-            </form>
-            <div className="active-project">
-              <p>
-                Active project: <span>PGM3</span>
-              </p>
-              <button className="btn add-task">Add new task</button>
-              <a href="./backlog.html" className="btn view-backlog">
-                View backlog
-              </a>
-            </div>
-          </header>
+          <Header />
 
           <Outlet />
           <TanStackRouterDevtools />
